62cda9003e998611ddf3e7c98be6cff0f61e502e
Isaac <ijsaffold@mail.lipscomb.edu>
2018-12-12 23:07:56 -0600

    made all constants long doubles

--- include/math_constants.h
+++ include/math_constants.h
@@ -3,12 +3,12 @@
 
 /* Contains macro definitions for several well-known mathematical constants. */
 
-#define PI    3.141592653589793
-#define TWOPI 6.283185307179586
-#define E     2.718281828459045
-#define PHI   1.618033988749895
-#define SQRT2 1.414213562373095
-#define LOG2  0.693147180559945
-#define LOG10 2.302585092994046
+#define PI    3.141592653589793L
+#define TWOPI 6.283185307179586L
+#define E     2.718281828459045L
+#define PHI   1.618033988749895L
+#define SQRT2 1.414213562373095L
+#define LOG2  0.693147180559945L
+#define LOG10 2.302585092994046L
 
 #endif

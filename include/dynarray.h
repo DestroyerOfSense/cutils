d821268d62b5af2c06527ff5902f03565af750a7
Isaac <ijsaffold@mail.lipscomb.edu>
2018-12-19 19:23:28 -0600

    second draft of dynamic array implementation

--- include/dynarray.h
+++ include/dynarray.h
@@ -1,21 +1,51 @@
 #ifndef DYNARRAY_H
 #define DYNARRAY_H
 
-#include <stddef.h>
+#include <string.h>
 
-struct DynArray;
+struct DynArray
+{
+	void* data;
+	size_t elemCount;
+	size_t len;
+};
 
-struct DynArray* dyn_init(struct DynArray* arr, size_t len, size_t elemSize);
-void dyn_reset(struct DynArray* arr);
-struct DynArray* dyn_setElemCount(struct DynArray* arr, size_t elemCount, size_t elemSize);
-struct DynArray* dyn_incrElemCount(struct DynArray* arr, size_t elemSize);
-void dyn_shrinkToFit(struct DynArray* arr, size_t elemSize);
-void dyn_append(struct DynArray* dst, const void* restrict src, size_t srcLen, size_t elemSize);
+void* dyn_setElemCount(struct DynArray* arr, size_t elemCount, size_t elemSize);
+void* dyn_shrinkToFit(struct DynArray* arr, size_t elemSize);
 void dyn_insert(struct DynArray* dst, const void* src, size_t pos, size_t srcLen, size_t elemSize);
 
-#define dyn_push(arr, elem)                                               \
-{                                                                         \
-	dyn_incrElemCount(arr, sizeof elem)->data[arr->elemCount - 1] = elem; \
+inline struct DynArray* dyn_init(struct DynArray* arr, size_t elemSize)
+{
+	arr->data = malloc(elemSize << 3);
+	arr->elemCount = 0;
+	arr->len = 8;
+	return arr;
+}
+
+inline void dyn_reset(struct DynArray* arr)
+{
+	free(arr->data);
+	arr->elemCount = 0;
+	arr->len = 0;
+}
+
+inline void* dyn_incrElemCount(struct DynArray* arr, size_t elemSize)
+{
+	dyn_setElemCount(arr, arr->elemCount + 1, elemSize);
+}
+
+inline void dyn_append(struct DynArray* dst, const void* src, size_t srcLen, size_t elemSize)
+{
+	size_t oldElemCount = dst->elemCount;
+	char* elems = dyn_setElemCount(dst, dst->elemCount + srcLen, elemSize);
+	if (elems)
+		memmove(elems + elemSize*oldElemCount, src, elemSize * srcLen);
+}
+
+inline void dyn_remove(struct DynArray* arr, size_t from, size_t to, size_t elemSize)
+{
+	memmove((char*)arr->data + from*elemSize, (char*)arr->data + to*elemSize, (arr->elemCount - to) * elemSize);
+	arr->elemCount -= (to - from);
 }
 
 #endif
--- src/dynarray.c
+++ src/dynarray.c
@@ -3,82 +3,59 @@
 #include <math.h>
 
 #include "math_constants.h"
+#include "dynarray.h"
 
 #define GROWTH_FACTOR PHI
 
-struct DynArray
-{
-	void* data;
-	size_t elemCount;
-	size_t len;
-};
 typedef struct DynArray DynArray;
 
-DynArray* dyn_init(DynArray* arr, size_t len, size_t elemSize)
-{
-	arr->data = malloc(len * elemSize);
-	arr->elemCount = 0;
-	arr->len = len;
-	return arr;
-}
+extern inline DynArray* dyn_init(DynArray* arr, size_t elemSize);
+extern inline void dyn_reset(DynArray* arr);
+extern inline void* dyn_incrElemCount(DynArray* arr, size_t elemSize);
+extern inline void dyn_append(DynArray* dst, const void* src, size_t srcLen, size_t elemSize);
+extern inline void dyn_remove(DynArray* arr, size_t from, size_t to, size_t elemSize);
 
-void dyn_reset(DynArray* arr)
+void* dyn_setElemCount(DynArray* arr, size_t to, size_t elemSize)
 {
-	free(arr->data);
-	arr->elemCount = 0;
-	arr->len = 0;
-}
-
-DynArray* dyn_setElemCount(DynArray* arr, size_t elemCount, size_t elemSize)
-{
-	if (elemCount > arr->len)
+	if (to > arr->len)
 	{
-		size_t len = fmax(elemCount, round(GROWTH_FACTOR * arr->len));
-		void* data = realloc(arr->data, len * elemSize);
-		if (data)
-			arr->data = data, arr->len = len;
+		size_t newLen = fmax(to, round(GROWTH_FACTOR * arr->len));
+		void* newData = realloc(arr->data, newLen * elemSize);
+		if (newData)
+			arr->data = newData, arr->len = newLen;
 		else
 		{
 			free(arr->data);
 			arr->data = NULL;
 		}
 	}
-	arr->elemCount = elemCount;
-	return arr;
+	arr->elemCount = to;
+	return arr->data;
 }
 
-DynArray* dyn_incrElemCount(DynArray* arr, size_t elemSize)
-{
-	return dyn_setElemCount(arr, arr->elemCount + 1, elemSize);
-}
-
-void dyn_shrinkToFit(DynArray* arr, size_t elemSize)
+void* dyn_shrinkToFit(DynArray* arr, size_t elemSize)
 {
 	if (arr->len > arr->elemCount)
 	{
-		void* data = realloc(arr->data, arr->elemCount * elemSize);
-		if (data)
-			arr->data = data, arr->len = arr->elemCount;
+		void* newData = realloc(arr->data, arr->elemCount * elemSize);
+		if (newData)
+			arr->data = newData, arr->len = arr->elemCount;
 		else
 		{
 			free(arr->data);
 			arr->data = NULL;
 		}
 	}
+	return arr->data;
 }
 
 void dyn_insert(DynArray* dst, const void* src, size_t pos, size_t srcLen, size_t elemSize)
 {
-	if (!dyn_setElemCount(dst, dst->elemCount + srcLen, elemSize)->data)
-		return;
-	void* start = dst->data + pos;
-	memmove(start + srcLen, start, (dst->elemCount - pos) * elemSize);
-	memmove(start, src, srcLen * elemSize);
-}
-
-void dyn_append(DynArray* dst, const void* restrict src, size_t srcLen, size_t elemSize)
-{
-	void* end = dst->data + dst->elemCount;
-	if (dyn_setElemCount(dst, dst->elemCount + srcLen, elemSize)->data)
-		memcpy(end, src, srcLen * elemSize);
+	size_t oldElemCount = dst->elemCount;
+	if (dyn_setElemCount(dst, dst->elemCount + srcLen, elemSize))
+	{
+		char* start = (char*)dst->data + elemSize*pos;
+		memmove(start + elemSize*srcLen, start, (oldElemCount - pos) * elemSize);
+		memmove(start, src, srcLen * elemSize);
+	}
 }

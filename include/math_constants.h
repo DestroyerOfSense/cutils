dab552d63963addec0c2871ab030319eb8eb0ff4
Isaac <ijsaffold@mail.lipscomb.edu>
2019-03-29 21:59:25 -0500

    added descriptions of lesser-known constants

--- include/math_constants.h
+++ include/math_constants.h
@@ -6,9 +6,9 @@
 #define PI    3.141592653589793L
 #define TWOPI 6.283185307179586L
 #define E     2.718281828459045L
-#define PHI   1.618033988749895L
+#define PHI   1.618033988749895L	// golden ratio
 #define SQRT2 1.414213562373095L
-#define LOG2  0.693147180559945L
-#define LOG10 2.302585092994046L
+#define LOG2  0.693147180559945L	// natural logarithm of two
+#define LOG10 2.302585092994046L	// natural logarithm of ten
 
 #endif

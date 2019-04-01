e52ee96eb6b6635eb7e702cb870c4c23250ce0f4
Isaac <ijsaffold@mail.lipscomb.edu>
2019-03-31 23:04:23 -0500

    changed member names, made 'dyn_append' use 'memcpy' rather than 'memmove'

--- include/dynarray.h
+++ include/dynarray.h
@@ -7,46 +7,46 @@
 struct DynArray
 {
 	void* data;
-	size_t elemCount;
 	size_t len;
+	size_t capacity;
 };
 
-void* dyn_setElemCount(struct DynArray* arr, size_t elemCount, size_t elemSize);
+void* dyn_setLen(struct DynArray* arr, size_t len, size_t elemSize);
 void* dyn_shrinkToFit(struct DynArray* arr, size_t elemSize);
 void dyn_insert(struct DynArray* dst, const void* src, size_t pos, size_t srcLen, size_t elemSize);
 
 inline struct DynArray* dyn_init(struct DynArray* arr, size_t elemSize)
 {
 	arr->data = malloc(elemSize << 3);
-	arr->elemCount = 0;
-	arr->len = 8;
+	arr->len = 0;
+	arr->capacity = 8;
 	return arr;
 }
 
 inline void dyn_reset(struct DynArray* arr)
 {
 	free(arr->data);
-	arr->elemCount = 0;
 	arr->len = 0;
+	arr->capacity = 0;
 }
 
-inline void* dyn_incrElemCount(struct DynArray* arr, size_t elemSize)
+inline void* dyn_incrLen(struct DynArray* arr, size_t elemSize)
 {
-	return dyn_setElemCount(arr, arr->elemCount + 1, elemSize);
+	return dyn_setLen(arr, arr->len + 1, elemSize);
 }
 
-inline void dyn_append(struct DynArray* dst, const void* src, size_t srcLen, size_t elemSize)
+inline void dyn_append(struct DynArray* restrict dst, const void* restrict src, size_t srcLen, size_t elemSize)
 {
-	size_t oldElemCount = dst->elemCount;
-	char* elems = dyn_setElemCount(dst, dst->elemCount + srcLen, elemSize);
+	size_t oldLen = dst->len;
+	char* elems = dyn_setLen(dst, dst->len + srcLen, elemSize);
 	if (elems)
-		memmove(elems + elemSize*oldElemCount, src, elemSize * srcLen);
+		memcpy(elems + elemSize*oldLen, src, elemSize * srcLen);
 }
 
 inline void dyn_remove(struct DynArray* arr, size_t from, size_t to, size_t elemSize)
 {
-	memmove((char*)arr->data + from*elemSize, (char*)arr->data + to*elemSize, (arr->elemCount - to) * elemSize);
-	arr->elemCount -= (to - from);
+	memmove((char*)arr->data + from*elemSize, (char*)arr->data + to*elemSize, (arr->len - to) * elemSize);
+	arr->len -= (to - from);
 }
 
 #endif
--- src/dynarray.c
+++ src/dynarray.c
@@ -11,35 +11,35 @@ typedef struct DynArray DynArray;
 
 extern inline DynArray* dyn_init(DynArray* arr, size_t elemSize);
 extern inline void dyn_reset(DynArray* arr);
-extern inline void* dyn_incrElemCount(DynArray* arr, size_t elemSize);
+extern inline void* dyn_incrLen(DynArray* arr, size_t elemSize);
 extern inline void dyn_append(DynArray* dst, const void* src, size_t srcLen, size_t elemSize);
 extern inline void dyn_remove(DynArray* arr, size_t from, size_t to, size_t elemSize);
 
-void* dyn_setElemCount(DynArray* arr, size_t to, size_t elemSize)
+void* dyn_setLen(DynArray* arr, size_t to, size_t elemSize)
 {
-	if (to > arr->len)
+	if (to > arr->capacity)
 	{
-		size_t newLen = fmax(to, round(GROWTH_FACTOR * arr->len));
+		size_t newLen = fmax(to, round(GROWTH_FACTOR * arr->capacity));
 		void* newData = realloc(arr->data, newLen * elemSize);
 		if (newData)
-			arr->data = newData, arr->len = newLen;
+			arr->data = newData, arr->capacity = newLen;
 		else
 		{
 			free(arr->data);
 			arr->data = NULL;
 		}
 	}
-	arr->elemCount = to;
+	arr->len = to;
 	return arr->data;
 }
 
 void* dyn_shrinkToFit(DynArray* arr, size_t elemSize)
 {
-	if (arr->len > arr->elemCount)
+	if (arr->capacity > arr->len)
 	{
-		void* newData = realloc(arr->data, arr->elemCount * elemSize);
+		void* newData = realloc(arr->data, arr->len * elemSize);
 		if (newData)
-			arr->data = newData, arr->len = arr->elemCount;
+			arr->data = newData, arr->capacity = arr->len;
 		else
 		{
 			free(arr->data);
@@ -51,11 +51,11 @@ void* dyn_shrinkToFit(DynArray* arr, size_t elemSize)
 
 void dyn_insert(DynArray* dst, const void* src, size_t pos, size_t srcLen, size_t elemSize)
 {
-	size_t oldElemCount = dst->elemCount;
-	if (dyn_setElemCount(dst, dst->elemCount + srcLen, elemSize))
+	size_t oldLen = dst->len;
+	if (dyn_setLen(dst, dst->len + srcLen, elemSize))
 	{
 		char* start = (char*)dst->data + elemSize*pos;
-		memmove(start + elemSize*srcLen, start, (oldElemCount - pos) * elemSize);
+		memmove(start + elemSize*srcLen, start, (oldLen - pos) * elemSize);
 		memmove(start, src, srcLen * elemSize);
 	}
 }

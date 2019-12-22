7447b3eecf1572361795860b7612bdde493b78de
Isaac <ijsaffold@mail.lipscomb.edu>
2019-12-21 18:22:24 -0600

    prefixed math constants with 'math_'

--- include/cutils/math/constants.h
+++ include/cutils/math/constants.h
@@ -3,12 +3,12 @@
 
 /* Contains macro definitions for several well-known mathematical constants. */
 
-#define PI    3.141592653589793
-#define TWOPI 6.283185307179586
-#define E     2.718281828459045
-#define PHI   1.618033988749895	    // golden ratio
-#define SQRT2 1.414213562373095
-#define LOG2  0.6931471805599453    // natural logarithm of two
-#define LOG10 2.302585092994046     // natural logarithm of ten
+#define MATH_PI    3.141592653589793
+#define MATH_TWOPI 6.283185307179586
+#define MATH_E     2.718281828459045
+#define MATH_PHI   1.618033988749895     // golden ratio
+#define MATH_SQRT2 1.414213562373095
+#define MATH_LOG2  0.6931471805599453    // natural logarithm of two
+#define MATH_LOG10 2.302585092994046     // natural logarithm of ten
 
 #endif

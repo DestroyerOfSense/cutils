56298cf6ae799032a598716dacc4978576d029db
Isaac <ijsaffold@mail.lipscomb.edu>
2020-02-24 15:15:47 -0600

    finished documenting generic 'DynArray'

--- include/cutils/data_structures/dyn_array_g.h
+++ include/cutils/data_structures/dyn_array_g.h
@@ -1,17 +1,79 @@
 #ifndef CUTILS_DATA_STRUCTURES_DYN_ARRAY_G_H_12212019
 #define CUTILS_DATA_STRUCTURES_DYN_ARRAY_G_H_12212019
 
+/** @file
+ * @brief Contains a generic version of `ctls_DynArray`.
+ *
+ * This file contains macros that facilitate the creation of specializations of `ctls_DynArray` for different types
+ * in a manner similar to templates in C++. In addition to often being more convenient than the use of void pointers,
+ * this allows compilers to optimize more effectively.
+ *
+ * Each macro takes a `type` and optional `suffix` argument. `type` is simply the type to be specialized for, and
+ * `suffix` is an optional suffix to be appended to every name associated with the specialization. If multiple
+ * specializations are created in a single program, or if the non-generic `ctls_DynArray` is used, each specialization
+ * should have a different suffix, to avoid name conflicts. Here's an example:
+ *
+ * @code
+ * // The corresponding definitions are located in another file.
+ * CTLS_DYN_ARRAY_DECL(int, _int)
+ *
+ * void f(void)
+ * {
+ *     ctls_DynArray_int arr;
+ *     ctls_dyn_init_int(&arr);
+ *     // ...
+ * }
+ * @endcode
+ *
+ * The functions associated with a given specialization of `ctls_DynArray` are nearly identical to those declared in
+ * cutils/data_structures/dyn_array.h. Here are their differences:
+ *
+ * - The generic function's first parameter is a pointer to the specialization, rather than the non-generic
+ *   `ctls_DynArray`.
+ * - The generic function has no `elemSize` parameter.
+ * - For a specialization for type *T*, `ctls_dyn_append`'s parameter `elem` is of type *T*, rather than
+ *   `const void* restrict`.
+ *
+ * It should be noted that the functions and objects associated with a given specialization of `ctls_DynArray` have
+ * external linkage if the corresponding functions and objects are declared in cutils/data_structures/dyn_array.h.
+ */
+
+/// @cond
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 #include <math.h>
+/// @endcond
 
 #include "cutils/math/constants.h"
 
 #define CTLS_DYN_GROWTH_FACTOR (float)CTLS_PHI
 #define CTLS_DYN_DEFAULT_INITIAL_CAPACITY 8
 
+/**
+ * @brief Creates declarations for a specialization of `ctls_DynArray`.
+ * @param type the name of the type to be specialized for
+ * @param suffix a string appended to each declared identifier. Optional.
+ *
+ * Using this macro is equivalent to including a header file for a given specialization. The corresponding
+ * implementation is created via `CTLS_DYN_ARRAY_DEF`.
+ *
+ * If `suffix` is not provided, the corresponding identifiers are the same as those declared in
+ * cutils/data_structures/dyn_array.h.
+ *
+ * **Usage**
+ * @code
+ * CTLS_DYN_ARRAY_DECL(int, _int)
+ *
+ * void f(void)
+ * {
+ *     ctls_DynArray_int arr;
+ *     ctls_dyn_init_int(&arr);
+ *     // ...
+ * }
+ * @endcode
+ */
 #define CTLS_DYN_ARRAY_DECL(type, ...) \
 \
 struct ctls_DynArray##__VA_ARGS__ \
@@ -29,6 +91,23 @@ bool ctls_dyn_insert##__VA_ARGS__(struct ctls_DynArray##__VA_ARGS__* dynArr, typ
 bool ctls_dyn_extend##__VA_ARGS__(struct ctls_DynArray##__VA_ARGS__* dynArr, type const* src, size_t srcLen); \
 void ctls_dyn_remove##__VA_ARGS__(struct ctls_DynArray##__VA_ARGS__* dynArr, size_t from, size_t to);
 
+/**
+ * @brief Creates definitions for a specialization of `ctls_DynArray`.
+ * @param type the name of the type to be specialized for
+ * @param suffix a string appended to each declared identifier. Optional.
+ *
+ * The corresponding declarations can, and should, be included via `CTLS_DYN_ARRAY_DECL`.
+ *
+ * If `suffix` is not provided, the corresponding identifiers are the same as those declared in
+ * cutils/data_structures/dyn_array.h. In this case, if the implementation file for cutils/data_structures/dyn_array.h
+ * is present in the program, a name conflict will result.
+ *
+ * **Usage**
+ * @code
+ * CTLS_DYN_ARRAY_DECL(int, _int)
+ * CTLS_DYN_ARRAY_DEF(int, _int)
+ * @endcode
+ */
 #define CTLS_DYN_ARRAY_DEF(type, ...) \
 \
 static bool ctls_dyn_reallocData##__VA_ARGS__(struct ctls_DynArray##__VA_ARGS__* dynArr, size_t newCapacity) \
@@ -105,6 +184,16 @@ void ctls_dyn_remove##__VA_ARGS__(struct ctls_DynArray##__VA_ARGS__* dynArr, siz
     dynArr->size -= (to - from); \
 }
 
+/**
+ * @brief a convenience function that calls both `CTLS_DYN_ARRAY_DECL` and `CTLS_DYN_ARRAY_DEF`.
+ * @param type the name of the type to be specialized for
+ * @param suffix a string appended to each declared identifier. Optional.
+ *
+ * **Usage**
+ * @code
+ * CTLS_DYN_ARRAY(int, _int)
+ * @endcode
+ */
 #define CTLS_DYN_ARRAY(type, ...) \
 CTLS_DYN_ARRAY_DECL(type, __VA_ARGS__) \
 CTLS_DYN_ARRAY_DEF(type, __VA_ARGS__)
